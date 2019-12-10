ba0a1a7690b44210cd01029257a5ea7eee54387b
Quan Peng <peng.qu@husky.neu.edu>
2019-12-09 22:59:18 -0500

    login

--- final-project/src/app/app.module.ts
+++ final-project/src/app/app.module.ts
@@ -1,8 +1,11 @@
+// import { UserComponent } from './users/user.component';
+import { RegisterComponent } from './users/register/register.component';
+import { LoginComponent } from './users/login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
@@ -12,18 +15,23 @@ import { ContactComponent } from './contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegisterComponent,
     WelcomeComponent,
     ContactComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
 
     RouterModule.forRoot([
+      { path: 'login', component: LoginComponent},
+      { path: 'register', component: RegisterComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: 'contact', component: ContactComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '**', redirectTo: 'login', pathMatch: 'full' }
     ]),
     ProductModule
   ],
---
+++ final-project/src/app/users/user.service.ts
@@ -0,0 +1,39 @@
+import { User } from './user.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private remoteUrlPattern = 'http://localhost:3000/users';
+
+  constructor(private http: HttpClient) { }
+
+  public getAllUsers(): Observable<Array<User>> {
+    const users$ = this.http.get<User[]>(this.remoteUrlPattern);
+    return users$;
+  }
+
+  public getUserByUsername(username: string): Observable<Array<User>> {
+    const users$ = this.http.get<User[]>(this.remoteUrlPattern + `/${username}`);
+    return users$;
+  }
+
+  public getUserById(id: number): Observable<Array<User>> {
+    const users$ = this.http.get<User[]>(this.remoteUrlPattern + `${id}`);
+    return users$;
+  }
+
+  public saveNewUser(user: Object): Observable<User> {
+    const user$ = this.http.post<User>(this.remoteUrlPattern, user);
+    return user$;
+  }
+
+  public updateUser(user: User): Observable<User> {
+    const user$ = this.http.put<User>(this.remoteUrlPattern + `/${user.id}`, user);
+    return user$;
+  }
+}
