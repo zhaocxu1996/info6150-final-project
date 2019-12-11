a0479bf3e94a0246413ab81a5944da19551b3a18
Quan Peng <peng.qu@husky.neu.edu>
2019-12-11 00:58:32 -0500

    add peng's work

--- final-project/src/app/contactUs/contactUs.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './contactUs.component.html',
-  styleUrls: ['./contactUs.component.css']
-})
-export class contactUsComponent {
-  public pageTitle = 'contactUs';
-}
